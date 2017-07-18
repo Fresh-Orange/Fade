7a0fc7515843359cb6ed98a17858c6dc0551c6a9
huanglu20124 <845758437@qq.com>
2017-07-18 16:50:02 +0800

    测试了leancloud的短信注册api，添加了相关的包依赖

--- app/src/main/java/com/sysu/pro/fade/utils/Const.java
+++ app/src/main/java/com/sysu/pro/fade/utils/Const.java
@@ -5,10 +5,18 @@ package com.sysu.pro.fade.utils;
  */
 public class Const {
     public static final int PAGE_SIZE = 4; //页面数量
-    public static final String IP = "192.168.137.1:8080"; //ip地址和端口号
+    public static final String IP = "sysufade.cn"; //ip地址和端口号
     public static final String USER_SHARE="login_user"; //存储登录后的用户信息数据
     public static final int HOME = 1;
     public static final int DISCOVER = 2;
     public static final int MESSAGE = 3;
     public static final int MY = 4;
+
+    //用于leancloud服务器的常量
+    public static final String X_LC_ID = "flQyv4KhLIrCGcTQmKJTIigu-gzGzoHsz";
+    public static final String X_LC_KEY = "2qHqzvu8oqNzlcpiPE2zsRQW";
+    public static final String SEND_SMS_URL = "https://api.leancloud.cn/1.1/requestSmsCode";
+    public static final String CHECK_SMS_URL = "https://api.leancloud.cn/1.1/verifySmsCode/";
+    public static final String APP_ID = "flQyv4KhLIrCGcTQmKJTIigu-gzGzoHsz";
+    public static final String APP_KEY = "2qHqzvu8oqNzlcpiPE2zsRQW";
 }
