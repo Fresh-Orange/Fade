29f58714b4b8d28b3df992a5ff4a31a00e09cac1
baostorm <baostorm@163.com>
2017-07-22 22:42:30 +0800

    初步完善登录注册布局排版

--- app/src/main/java/com/sysu/pro/fade/my/activity/CheckTelActivity.java
+++ app/src/main/java/com/sysu/pro/fade/my/activity/CheckTelActivity.java
@@ -1,4 +1,4 @@
-﻿package com.sysu.pro.fade.my.activity;
+package com.sysu.pro.fade.my.activity;
 
 import android.content.Intent;
 import android.os.Handler;
@@ -31,10 +31,12 @@ public class CheckTelActivity extends AppCompatActivity {
                 String ans = (String) msg.obj;
                 Toast.makeText(CheckTelActivity.this,ans,Toast.LENGTH_SHORT).show();
                 if(ans.equals("{}")){
+
                     //验证成功，跳转到输入密码界面
                     Intent intent = new Intent(CheckTelActivity.this,AddPasswordActivity.class);
                     intent.putExtra(Const.TELEPHONE,mobilePhoneNumber);
                     startActivity(intent);
+
                     finish();
                 }
             }
