f3eb2f7ad2cbacf68bdd453851272067083a94de
baostorm <baostorm@163.com>
2017-08-04 18:20:25 +0800

    增加ignore的内容，忽略一些配置的文件修改

---
+++ app/src/main/java/com/sysu/pro/fade/my/BackBar.java
@@ -0,0 +1,10 @@
+package com.sysu.pro.fade.my;
+
+import android.widget.LinearLayout;
+
+/**
+ * Created by 12194 on 2017/8/4.
+ */
+
+public class BackBar extends LinearLayout {
+}
---
+++ app/src/main/java/com/sysu/pro/fade/my/setting/Personal.java
@@ -0,0 +1,15 @@
+package com.sysu.pro.fade.my.setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sysu.pro.fade.R;
+
+public class Personal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal);
+    }
+}
