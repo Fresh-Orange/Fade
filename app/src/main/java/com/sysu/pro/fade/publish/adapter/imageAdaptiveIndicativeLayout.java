d893177fa464cc96a5a3454ec67e3db7c0dd0ed1
yellow <410542141@qq.com>
2017-09-30 19:04:36 +0800

    9.30 更新裁剪功能

--- app/src/main/java/com/sysu/pro/fade/publish/adapter/imageAdaptiveIndicativeLayout.java
+++ app/src/main/java/com/sysu/pro/fade/publish/adapter/imageAdaptiveIndicativeLayout.java
@@ -18,7 +18,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-import android.widget.ViewSwitcher;
 
 import com.bumptech.glide.Glide;
 import com.sysu.pro.fade.R;
@@ -28,7 +27,7 @@ import com.sysu.pro.fade.publish.imageselector.ClickToPreviewActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.sysu.pro.fade.publish.PublishActivity.currentItem;
+
 import static com.sysu.pro.fade.publish.PublishActivity.publishActivity;
 
 /**
@@ -57,7 +56,6 @@ public class imageAdaptiveIndicativeLayout extends FrameLayout {
 	private int viewPagerMaxHeight = 400;
 	private mImageItemPagerAdapter imgAdapter;
 	private static ViewPager pager;
-	public static ViewSwitcher viewSwitcher;
 	public static MyCallBack myCallBack;
 	private LinearLayout dotLinearLayout;
 	public static ArrayList<String> images;
@@ -148,9 +146,6 @@ public class imageAdaptiveIndicativeLayout extends FrameLayout {
 		return  (int) (padding_in_dp * scale + 0.5f);
 	}
 
-	public void setViewSwitcher(ViewSwitcher viewSwitcher) {
-		this.viewSwitcher = viewSwitcher;
-	}
 
 	public void setCallBack(MyCallBack callBack) {
 		this.myCallBack = callBack;
@@ -232,16 +227,6 @@ public class imageAdaptiveIndicativeLayout extends FrameLayout {
 					Toast.makeText(publishActivity,"click",Toast.LENGTH_LONG).show();
 				}
 			});
-//			viewSwitcher = (ViewSwitcher) v.findViewById(R.id.view_switcher);
-//			mImageView.setOnLongClickListener(new OnLongClickListener() {
-//				@Override
-//				public boolean onLongClick(View v) {
-//					Toast.makeText(publishActivity, "Long click",
-//							Toast.LENGTH_LONG).show();
-//					viewSwitcher.setDisplayedChild(1);
-//					return false;
-//				}
-//			});
 			return v;
 		}
 
