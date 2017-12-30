9508c41f87e069a0a7e3573410dcaec5dbbb4546
baostorm <baostorm@163.com>
2017-12-30 20:35:03 +0800

    初步做出详情页的框架

--- app/src/main/java/com/sysu/pro/fade/home/activity/DetailActivity.java
+++ app/src/main/java/com/sysu/pro/fade/home/activity/DetailActivity.java
@@ -1,37 +1,31 @@
 package com.sysu.pro.fade.home.activity;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.sysu.pro.fade.Const;
 import com.sysu.pro.fade.R;
-import com.sysu.pro.fade.home.adapter.DetailFragmentAdapter;
-import com.sysu.pro.fade.home.fragment.DetailPageFragment;
-import com.sysu.pro.fade.home.view.ObservableScrollView;
+import com.sysu.pro.fade.beans.Comment;
+import com.sysu.pro.fade.home.adapter.CommonAdapter;
 
-public class DetailActivity extends AppCompatActivity implements ObservableScrollView.OnObservableScrollViewScrollChanged{
+import java.util.ArrayList;
+import java.util.List;
 
-    //详情页上下滑动
-    private ObservableScrollView sv_contentView;
-    private LinearLayout ll_topView; //包住悬浮布局的
-   // private TextView tv_topView;
-    private LinearLayout ll_tab;  //悬浮的布局
-    private LinearLayout ll_fixedView;
-    //用来记录内层固定布局到屏幕顶部的距离
-    private int mHeight;
-    //配置viewpager相关
+/*
+ * rebuild by VJ 2017.12.30
+ */
+
+public class DetailActivity extends AppCompatActivity{
+
     public Integer note_id;
-
-    private Toolbar toolbar;
+    private ImageView detailBack;   //返回按钮
+    private ImageView detailSetting;    //三个点按钮
+    private RecyclerView recyclerView;
+    private CommonAdapter<Comment> commentAdapter;
+    private List<Comment> commentator = new ArrayList<>();  //第一评论者列表
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,72 +33,50 @@ public class DetailActivity extends AppCompatActivity implements ObservableScrol
         setContentView(R.layout.activity_detail);
         //初始化note_id
         note_id = getIntent().getIntExtra(Const.NOTE_ID,0);
-        sv_contentView= (ObservableScrollView) findViewById(R.id.sv_contentView);
-        ll_topView= (LinearLayout) findViewById(R.id.ll_topView);
-        ll_tab = (LinearLayout) findViewById(R.id.ll_tab);
-        ll_fixedView= (LinearLayout) findViewById(R.id.ll_fixedView);
-        ll_fixedView.setVisibility(View.INVISIBLE);
 
-        toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        //Fragment+ViewPager+FragmentViewPager组合的使用
-        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_detail);
-        DetailFragmentAdapter adapter = new DetailFragmentAdapter(getSupportFragmentManager(),this);
-        viewPager.setAdapter(adapter);
-
-        //TabLayout
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_detail);
-        tabLayout.setupWithViewPager(viewPager);
-
-        sv_contentView.setOnObservableScrollViewScrollChanged(this);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //返回监听
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if(hasFocus){
-            //获取HeaderView的高度，当滑动大于等于这个高度的时候，需要把topView移除当前布局，放入到外层布局
-            mHeight=ll_topView.getTop();
-        }
-    }
-
-    /**
-     * @param l Current horizontal scroll origin. 当前滑动的x轴距离
-     * @param t Current vertical scroll origin. 当前滑动的y轴距离
-     * @param oldl Previous horizontal scroll origin. 上一次滑动的x轴距离
-     * @param oldt Previous vertical scroll origin. 上一次滑动的y轴距离
-     */
-    @Override
-    public void onObservableScrollViewScrollChanged(int l, int t, int oldl, int oldt) {
-        if (t >= mHeight) {
-            if (ll_tab.getParent() != ll_fixedView) {
-                ll_topView.removeView(ll_tab);
-                ll_fixedView.addView(ll_tab);
-                ll_fixedView.setVisibility(View.VISIBLE);
-                ll_fixedView.setAlpha(1.0f);
+        //放直接评论的adapter
+        commentAdapter = new CommonAdapter<Comment>(commentator) {
+            @Override
+            public int getLayoutId(int ViewType) {
+                return R.layout.item_comment;
             }
-        } else {
-            if (ll_tab.getParent() != ll_topView) {
-                ll_fixedView.removeView(ll_tab);
-                ll_fixedView.setVisibility(View.INVISIBLE);
-                ll_fixedView.setAlpha(0.0f);
-                ll_topView.addView(ll_tab);
+
+            @Override
+            public void convert(CommonAdapter.ViewHolder holder, Comment data, int position) {
+                Comment comment = commentator.get(position);
+                holder.setGoodImage(R.id.comment_detail_good, comment.getComment_isGood());
+                holder.setImage(R.id.comment_detail_head, comment.getHead_image_url());
+                holder.setText(R.id.comment_detail_name, comment.getNickname());
+                holder.setText(R.id.comment_detail_date, comment.getComment_time().toString());
+                holder.setText(R.id.comment_detail_content, comment.getComment_content());
+
+                List<Comment> respondent = getReplys(position); //评论者对应的回复者列表
+                //放回复内容的adapter
+                CommonAdapter<Comment> replyAdapter = new CommonAdapter<Comment>(respondent) {
+                    @Override
+                    public int getLayoutId(int ViewType) {
+                        return R.layout.item_reply;
+                    }
+
+                    @Override
+                    public void convert(ViewHolder holder, Comment data, int position) {
+
+                    }
+                };
+                holder.setReplyAdapter(R.id.comment_detail_reply, replyAdapter);
             }
-        }
+        };
+
+        recyclerView.setAdapter(commentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    //获取第pos个评论的所有回复
+    private List<Comment> getReplys(int pos) {
+        List<Comment> replys = new ArrayList<>();
+        // TODO: 2017/12/30获取
+        return replys;
     }
 
 }
---
+++ app/src/main/java/com/sysu/pro/fade/home/adapter/CommonAdapter.java
@@ -0,0 +1,119 @@
+package com.sysu.pro.fade.home.adapter;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.sysu.pro.fade.R;
+
+import java.util.List;
+
+/**
+ * Created by 12194 on 2017/12/17.
+ */
+
+public abstract class CommonAdapter<T> extends RecyclerView.Adapter<CommonAdapter.ViewHolder> {
+
+    private List<T> mItemsList;
+    private OnItemClickListener mListener;
+
+    public CommonAdapter(List<T> list) {
+        mItemsList = list;
+    }
+
+    public abstract int getLayoutId(int ViewType);
+
+    @Override
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        return ViewHolder.get(parent, getLayoutId(viewType));
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+
+        convert(holder, mItemsList.get(position), position);
+
+        if (mListener != null) {
+            holder.mConvertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mListener.onClick(holder.getAdapterPosition());
+                }
+            });
+            holder.mConvertView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    mListener.onLongClick(holder.getAdapterPosition());
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemsList.size();
+    }
+
+    public abstract void convert(ViewHolder holder, T data, int position);
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private SparseArray<View> mView;
+        private View mConvertView;
+        private ViewHolder(View v) {
+            super(v);
+            mConvertView = v;
+            mView = new SparseArray<>();
+        }
+        public static ViewHolder get(ViewGroup parent, int layoutId) {
+            View convertView = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+            return new ViewHolder(convertView);
+        }
+        public <T extends View> T getView(int id) {
+            View v = mView.get(id);
+            if (v == null) {
+                v = mConvertView.findViewById(id);
+                mView.put(id, v);
+            }
+            return (T)v;
+        }
+        public void setText(int id, String value) {
+            TextView view = getView(id);
+            view.setText(value);
+        }
+        public void setGoodImage(int id, boolean isGood) {
+            ImageView view = getView(id);
+            if (isGood) Glide.with(view.getContext()).load(R.drawable.isgood).into(view);
+            else Glide.with(view.getContext()).load(R.drawable.isnotgood).into(view);
+        }
+        public void setImage(int id, String imageUrl) {
+            ImageView view = getView(id);
+            Glide.with(view.getContext()).load(imageUrl).into(view);
+        }
+        public void setReplyAdapter(int id, RecyclerView.Adapter adapter) {
+            RecyclerView view = getView(id);
+            view.setAdapter(adapter);
+            view.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        }
+        public void onWidgetClick(int id, View.OnClickListener listener) {
+            View view = getView(id);
+            view.setOnClickListener(listener);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onClick(int position);
+        void onLongClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+}
