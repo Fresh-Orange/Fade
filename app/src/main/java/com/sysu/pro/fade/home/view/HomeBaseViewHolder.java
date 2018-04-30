f238d48cf33cc8c761f1b1c7c2ef5fc2b65656c4
yellow <410542141@qq.com>
2018-04-30 22:58:00 +0800

    4.30 更新跳转bug

--- app/src/main/java/com/sysu/pro/fade/home/view/HomeBaseViewHolder.java
+++ app/src/main/java/com/sysu/pro/fade/home/view/HomeBaseViewHolder.java
@@ -112,7 +112,7 @@ abstract public class HomeBaseViewHolder extends RecyclerView.ViewHolder {
 		setGoToDetailClickListener(context, itemView, bean);
 		setAddOrMinusListener(context, clickableProgressBar, userUtil, bean);
 		setCommentListener(context, clickableProgressBar, bean);
-		setOnUserClickListener(context, tvName, userAvatar, tvAtUser, bean);
+		setOnUserClickListener(context, tvName, userAvatar, tvAtUser, bean, false);
 
 	}
 
@@ -312,7 +312,8 @@ abstract public class HomeBaseViewHolder extends RecyclerView.ViewHolder {
 											  TextView tvName,
 											  ImageView userAvatar,
 											  @Nullable TextView tvAtUser,
-											  final Note bean) {
+											  final Note bean,
+											  final boolean isDetail) {
 		tvName.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -331,7 +332,8 @@ abstract public class HomeBaseViewHolder extends RecyclerView.ViewHolder {
 		userAvatar.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (bean.getRelayUserNum() > 1){
+				//解决详情页跳转问题
+				if (bean.getRelayUserNum() > 1 && !isDetail){
 					Intent i = new Intent(context, RelayUsersActivity.class);
 					i.putExtra(Const.NOTE_ENTITY, bean);
 					context.startActivity(i);
